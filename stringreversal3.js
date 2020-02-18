97c32457142d83cac801ca906acf4730c73c9df6
github username <sjami002@gmail.com>
2020-02-17 20:39:10 -0500

    str rev 3 alt

--- stringreversal3.js
+++ stringreversal3.js
@@ -1,3 +1,8 @@
 function reverseStr(str){
   return  str.split("").reduce((rev, char) => char + rev)
 }
+
+// Alternative solution: works with or without the initial value (in thsi case, empty str)
+// function reverseStr(str) {
+//     return str.split("").reduce((rev, char) => char + rev, "")
+// }
