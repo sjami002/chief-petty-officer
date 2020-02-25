dfca375978e66d85c92da62b7d2894e802374755
github username <sjami002@gmail.com>
2020-02-24 20:27:08 -0500

    vowels 1

--- sentenceCapitalize1.js
+++ sentenceCapitalize1.js
@@ -1,8 +1,13 @@
 function capitalize(str){
-    const words = [];
+    const words = []; // placeholder
 
-    for (let word of str.split(' ')){
+    for (let word of str.split(' ')){     // splitting words at spaces to create an array
         words.push(word[0].toUpperCase() + word.slice(1));
+        // for each word in the array
+            // uppercase the first letter
+            // join the first letter with the rest of the letter
+            // push it into the results array
     }
     return words.join(' ');
+    // join the words array back into a string and return them
 }
---
+++ vowels1.js
@@ -0,0 +1,11 @@
+function vowels(str){
+    let count  = 0;
+    const checker = ['a', 'e', 'i', 'o', 'u'];
+
+    for (let char of str.toLowerCase()){
+        if(checker.includes(char)){
+            count++;
+        }
+    }
+    return count;
+}
