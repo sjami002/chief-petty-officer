8eff7c18ae75075caa6debb4abb3b2f2290819b3
github username <sjami002@gmail.com>
2020-02-27 18:53:34 -0500

    queue with peek

---
+++ weave/queue.js
@@ -0,0 +1,17 @@
+class Queue = {
+    constructor() {
+        this.data = [];
+    }
+
+    add(record){
+        this.data.unshift(record);
+    }
+
+    remove(){
+        this.data.pop();
+    }
+
+    peek() {
+        return this.data[this.data.length - 1];
+    }
+}
