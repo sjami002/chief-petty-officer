61e50ece3a7dc4f17fb66ebe06eaf85db3818510
github username <sjami002@gmail.com>
2020-02-28 14:34:54 -0500

    reestablish peek

--- weave/queue.js
+++ weave/queue.js
@@ -7,10 +7,15 @@ class Queue {
         this.data.unshift(record);
     }
 
+    peek() {
+        return this.data[this.data.length - 1];
+    }
+
     remove() {
         return this.data.pop();
     }
 
+    
 }
 
 module.exports = Queue;
